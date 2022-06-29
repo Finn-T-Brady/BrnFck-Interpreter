726d00ced6433e6b405fef2e63a83ff157811419
Finn Brady <finn.t.brady@gmail.com>
2022-06-29 22:27:35 +0100

    Update Brain.h

--- Brain.h
+++ Brain.h
@@ -8,37 +8,49 @@ ascii of valid instructions
 ] : 93 : if(*mem) while(*exec!=91) exec--
 . : 46 : putchar(*mem)
 , : 44 : getchar(*mem)
+
+in order of ascii value
+43 inc
+44 read
+45 dec
+46 put
+60 left
+62 right
+91 open
+93 close  --  irrelevant during index calculation
+
+index = (*mem>43) + (*mem>44) + (*mem>45) + (*mem>46) + (*mem>60) + (*mem>62) + (*mem>91)
 */
 
 //https://stackoverflow.com/questions/252748/how-can-i-use-an-array-of-function-pointers
 
-static inline void inc(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_inc(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	++*mem;
 	#ifdef DEBUG
         printf("%u, %c",*exec,*exec);
         #endif
 }
-static inline void dec(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_dec(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	--*mem;
         #ifdef DEBUG
         printf("%u, %c",*exec,*exec);
         #endif
 }
-static inline void right(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_right(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	++mem;
         #ifdef DEBUG
         ++m;
         printf("%u, %c",*exec,*exec);
         #endif
 }
-static inline void left(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_left(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	--mem;
         #ifdef DEBUG
         ++m;
         printf("%u, %c",*exec,*exec);
         #endif
 }
-static inline void open(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_open(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	#ifdef DEBUG
 	printf("%u, %c",*exec,*exec);
 	#endif
@@ -52,7 +64,7 @@ static inline void open(volatile unsigned int *mem, volatile char *exec,unsigned
 			}
 	}
 }
-static inline void close(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_close(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	#ifdef DEBUG
 	printf("%u, %c",*exec,*exec);
 	#endif
@@ -63,13 +75,13 @@ static inline void close(volatile unsigned int *mem, volatile char *exec,unsigne
 	--stackTop;
 	f(*mem)exec=*stackTop-1;
 }
-static inline void put(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_put(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	#ifdef DEBUG
 	printf("%u, %c : ",*exec,*exec);
 	#endif
 	putchar(*mem);
 }
-static inline void read(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
+static inline void br_read(volatile unsigned int *mem, volatile char *exec,unsigned short int bridge, char** loopStack, char** stackTop){
 	#ifdef DEBUG
 	printf("%u, %c : ",*exec,*exec);
 	#endif
@@ -83,14 +95,14 @@ unsigned int m;
 	
 int Brfk(volatile char *exec, volatile unsigned int *mem){
 	
-    op[0] = inc;
-    op[1] = dec;
-    op[2] = right;
-    op[3] = left;
-    op[4] = open;
-    op[5] = close;
-    op[6] = put;
-    op[7] = read;
+    op[0] = br_inc;
+    op[1] = br_dec;
+    op[2] = br_right;
+    op[3] = br_left;
+    op[4] = br_open;
+    op[5] = br_close;
+    op[6] = br_put;
+    op[7] = br_read;
     #ifdef DEBUG
     m=0;
     int start = exec;
